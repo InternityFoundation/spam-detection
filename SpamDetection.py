7b0412ba4377a37e3f688fa6fb589541597ec554
Shivam Gupta <shivam270896@gmail.com>
2017-07-19 20:06:20 +0530

    Training our classifier using Naive Bayes and SVM

--- SpamDetection.py
+++ SpamDetection.py
@@ -47,3 +47,19 @@ def extract_features(mail_dir):
                   features_matrix[docID,wordID] = words.count(word)
         docID = docID + 1     
     return features_matrix
+
+## Training our classifier using Naive Bayes and SVM
+# Create a dictionary of words with its frequency
+train_dir = 'train-mails'
+dictionary = make_Dictionary(train_dir)
+
+# Prepare feature vectors per training mail and its labels
+train_labels = np.zeros(702)
+train_labels[351:701] = 1
+train_matrix = extract_features(train_dir)
+
+# Training SVM and Naive bayes classifier
+model1 = MultinomialNB()
+model2 = LinearSVC()
+model1.fit(train_matrix,train_labels)
+model2.fit(train_matrix,train_labels)
