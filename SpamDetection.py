8d86dd7f23b5cb7b1848fb6b7c8f202456a6866a
Shivam Gupta <shivam270896@gmail.com>
2017-07-19 20:03:06 +0530

    Importing Required Packages

--- SpamDetection.py
+++ SpamDetection.py
@@ -1,1 +1,9 @@
+## Importing required packages
+import os
+import numpy as np
+from collections import Counter
+from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
+from sklearn.svm import SVC, NuSVC, LinearSVC
+from sklearn.metrics import confusion_matrix 
+
 
