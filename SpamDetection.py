91eee3e6f91152b7b7e32db35cffba0aaf6a56d1
Shivam Gupta <shivam270896@gmail.com>
2017-07-22 16:40:59 +0530

    Fixed Bugs and Improved Presentation of Notebook

--- SpamDetection.py
+++ SpamDetection.py
@@ -5,13 +5,13 @@ from collections import Counter
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.svm import SVC, NuSVC, LinearSVC
+from sklearn.svm import LinearSVC
 from sklearn.metrics import confusion_matrix
 
-## Creating a word dictionary
+## Function for creating a word dictionary
 def make_Dictionary(train_dir):
     
-    email_files = [os.path.join(train_dir,f) for f in os.listdir(train_dir)]
+    email_files = [os.path.join(train_dir,f) for f in sorted(os.listdir(train_dir))]
     stop_words = set(stopwords.words('english'))
     all_words = []       
     
@@ -34,15 +34,15 @@ def make_Dictionary(train_dir):
         elif item in stop_words:
             del dictionary[item]
     
-    dictionary = dictionary.most_common(3000)
+    dictionary = dictionary.most_common(5000)
     
     return dictionary
 
-## Feature Extraction Process
-def extract_features(train_dir): 
+## Function for the process of Feature Extraction
+def extract_features(mail_dir): 
     
-    email_files = [os.path.join(train_dir,f) for f in os.listdir(train_dir)]
-    features_matrix = np.zeros((len(email_files),3000))
+    email_files = [os.path.join(mail_dir,f) for f in sorted(os.listdir(mail_dir))]
+    features_matrix = np.zeros((len(email_files),5000))
     docID = 0
     
     for mail in email_files:
@@ -62,32 +62,37 @@ def extract_features(train_dir):
         docID = docID + 1     
     return features_matrix
 
-## Training our classifier using Naive Bayes and SVM
-# Create a dictionary of words with its frequency
+## Create a dictionary of words with its frequency
 train_dir = 'train-mails'
 dictionary = make_Dictionary(train_dir)
 
-# Prepare feature vectors per training mail and its labels
+## Prepare feature vectors of each training mail and its labels
 train_labels = np.zeros(702)
 train_labels[351:701] = 1
+
 train_matrix = extract_features(train_dir)
 
-# Training SVM and Naive bayes classifier
-model1 = MultinomialNB(alpha=0.5)
-model2 = LinearSVC()
+## Training Naive Bayes and SVM classifier
+NBmodel = MultinomialNB()
+NBmodel.fit(train_matrix,train_labels)
 
-model1.fit(train_matrix,train_labels)
-model2.fit(train_matrix,train_labels)
+SVMmodel = LinearSVC()
+SVMmodel.fit(train_matrix,train_labels)
 
-## Predicting test dataset using our classifiers
-# Test the unseen mails for Spam
+## Predicting the type of mail of test dataset using our models
 test_dir = 'test-mails'
-test_matrix = extract_features(test_dir)
+
 test_labels = np.zeros(260)
 test_labels[130:260] = 1
-result1 = model1.predict(test_matrix)
-result2 = model2.predict(test_matrix)
+
+test_matrix = extract_features(test_dir)
+
+NBmodelresult = NBmodel.predict(test_matrix)
+SVMmodelresult = SVMmodel.predict(test_matrix)
 
 ## Finding out accuracy of our models
-print confusion_matrix(test_labels,result1)
-print confusion_matrix(test_labels,result2)
+print 'Confusion Matrix of our Naive Bayes Classifier is:'
+print confusion_matrix(test_labels,NBmodelresult)
+
+print '\nConfusion Matrix of our SVM Classifier is:'
+print confusion_matrix(test_labels,SVMmodelresult)
